abf61ad66d1e53892af98c29e3562ea60ba05f09
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2021-08-23 02:23:59 -0400

    update: link

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,39 +1,43 @@
-import bookPunditsImg from '././assets/projects/the-book-pundits/cover.png'
-import techSapienImg from '././assets/projects/tech-sapien/cover.png'
-import tailwindGradientsImg from '././assets/projects/tailwind-gradient/cover.png'
-import portfolioImg from '././assets/projects/portfolio/cover.png'
+import bookPunditsImg from "././assets/projects/the-book-pundits/cover.png";
+import techSapienImg from "././assets/projects/tech-sapien/cover.png";
+import tailwindGradientsImg from "././assets/projects/tailwind-gradient/cover.png";
+import portfolioImg from "././assets/projects/portfolio/cover.png";
 
 export default [
-    {
-        id: 0,
-        img: bookPunditsImg,
-        heading: "The Book Pundits - Connecting Authors & Influencers",
-        shortDescription: "Book Pundits are a book advertising company located in Chennai, India. Book Pundits are a book advertising company located in Chennai, India. Book Pundits are a book advertising company located in Chennai, India.",
-        aboutLink: './thebookpundits',
-        liveLink: 'https://thebookpundits.com'
-    },
-    {
-        id: 1,
-        img: techSapienImg,
-        heading: "Tech Sapien - An interactive blog to learn web development",
-        shortDescription: "Tech sapien is a community targeted towards the programmers who strive for efficiency. The mind behind tech sapien is Shubham Chopade. He is a motivated individual whose passion is teaching. Visit the blog for more information.",
-        aboutLink: './techsapien',
-        liveLink: 'https://techsapien.org'
-    },
-    {
-        id: 2,
-        img: tailwindGradientsImg,
-        heading: "Tailwind Gradients Tool",
-        shortDescription: "An interactive tool for letting the developers choose gradient classes for tailwindCSS, a CSS framework. The users can generate a random gradient combination by pressing “Space”.",
-        aboutLink: './tailwindgradient',
-        liveLink: 'https://tailwindgradient.ml'
-    },
-    {
-        id: 3,
-        img: portfolioImg,
-        heading: "Portfolio Website - Self",
-        shortDescription: "Well this is also a long awaited project that I have been working for quite some time now. Find my design and development process alongwith. This website went through many modifications over the time. Here is the latest one. Hope you enjoy it.",
-        aboutLink: './portfolio',
-        liveLink: 'https://shubhamchopade.ml'
-    },
-]
+  {
+    id: 0,
+    img: bookPunditsImg,
+    heading: "The Book Pundits - Connecting Authors & Influencers",
+    shortDescription:
+      "Book Pundits are a book advertising company located in Chennai, India. Book Pundits are a book advertising company located in Chennai, India. Book Pundits are a book advertising company located in Chennai, India.",
+    aboutLink: "./thebookpundits",
+    liveLink: "https://thebookpundits.com",
+  },
+  {
+    id: 1,
+    img: techSapienImg,
+    heading: "Tech Sapien - An interactive blog to learn web development",
+    shortDescription:
+      "Tech sapien is a community targeted towards the programmers who strive for efficiency. The mind behind tech sapien is Shubham Chopade. He is a motivated individual whose passion is teaching. Visit the blog for more information.",
+    aboutLink: "./techsapien",
+    liveLink: "https://techsapien.dev",
+  },
+  {
+    id: 2,
+    img: tailwindGradientsImg,
+    heading: "Tailwind Gradients Tool",
+    shortDescription:
+      "An interactive tool for letting the developers choose gradient classes for tailwindCSS, a CSS framework. The users can generate a random gradient combination by pressing “Space”.",
+    aboutLink: "./tailwindgradient",
+    liveLink: "https://tailwindgradient.ml",
+  },
+  {
+    id: 3,
+    img: portfolioImg,
+    heading: "Portfolio Website - Self",
+    shortDescription:
+      "Well this is also a long awaited project that I have been working for quite some time now. Find my design and development process alongwith. This website went through many modifications over the time. Here is the latest one. Hope you enjoy it.",
+    aboutLink: "./portfolio",
+    liveLink: "https://shubhamchopade.ml",
+  },
+];
